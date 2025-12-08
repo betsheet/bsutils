961c99689945b27052bac6a48a23a3e531446e64
Adrián R. Espinosa <espinosa012@gmail.com>
2025-12-08 19:15:58 +0100

    new user models

--- bsutils/apimodels/user.py
+++ bsutils/apimodels/user.py
@@ -5,7 +5,15 @@ class BSUserCredentials(BSBaseEntity):
     username: str
     password: str
 
+class UserStakeConfig(BSBaseEntity):
+    pass
+
+class UserTelegramData(BSBaseEntity):
+    telegram_user_id: str
+    phone_number: str    # with country code, e.g., +34123456789
 
 class BSUser(BSBaseEntity):
     email: str | None
     credentials: BSUserCredentials
+    stake_config: UserStakeConfig | None
+    telegram_data: UserTelegramData | None

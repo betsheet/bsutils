3fe512a4fc6802af7cb2fe5c1219c02d3b9d2b69
espinosa012 <espinosa012@gmail.com>
2026-03-28 17:02:03 +0100

    BSTelegramMessage field telegram_message_id

--- bsutils/apimodels/pick_message.py
+++ bsutils/apimodels/pick_message.py
@@ -2,6 +2,8 @@ from bsutils.base.base import BSBaseEntity
 
 
 class BSTelegramMessage(BSBaseEntity):
+    telegram_message_id: str
+    message: str
     from_user_id: str  # id telegram del usuario que envía el mensaje (el que aporta el pick)
     from_telegram_chat_id: str  # id telegram del chat del que procede el mensaje
     content: str
